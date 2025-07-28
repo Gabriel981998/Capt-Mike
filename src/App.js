7036d0616ffc7356ca15f37fa74bb50722396064
gabe <105863473+Gabriel981998@users.noreply.github.com>
2025-07-28 21:20:11 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ import { FaArrowAltCircleRight } from "react-icons/fa";
 
 
 const LIST_DATA=[
-  {id:"1" ,value:"Dispatch / Load Sheet..........................ACQUIRED"},
-  {id:"2" ,value:"DC Voltmeter Selector...................................BAT"},
+  {id:"1" ,value:"Dispatch / Load Sheet.............ACQUIRED"},
+  {id:"2" ,value:"DC Voltmeter Selector......................BAT"},
   {id:"3" ,value:"DC Battery Switch..........................ON/GUARDED"},
   {id:"4" ,value:"Standby Power Switch.................AUTO/GUARDED"},
   {id:"5" ,value:"Alternate Flaps.............................OFF/GUARDED"},
@@ -15,18 +15,18 @@ const LIST_DATA=[
   {id:"7" ,value:"Wipers.......................................PARK"},
   {id:"8" ,value:"Hydraulic Pumps .......................OFF"},
   {id:"9" ,value:"Landing Gear Lever..................DOWN"},
-  {id:"10" ,value:"Weather Radar..........................................TEST"},
+  {id:"10" ,value:"Weather Radar............................TEST"},
   {id:"11" ,value:"Fire Warning System.............TEST"},
   {id:"12" ,value:"Ground Power or APU.........................CONNECT"},
   {id:"13" ,value:"AC Voltmeter Selector................GRD PWR or APU"},
-  {id:"14" ,value:"Electrical Panel.................................CHECK/SET"},
+  {id:"14" ,value:"Electrical Panel.......................CHECK/SET"},
   {id:"15" ,value:"IRS Selector Switches ............ALIGN/NAV"},
-  {id:"16" ,value:"Emergency Exit Lights..............................ARMED"},
+  {id:"16" ,value:"Emergency Exit Lights.....................ARMED"},
   {id:"17" ,value:"Air Conditioning Panel....................AS REQUIRED"},
   {id:"18" ,value:"CDU........................ENTER PRESENT POSITION"},
   {id:"19" ,value:"Flaps Lever Up.........................VERIFY POSITION"},
   {id:"20" ,value:"Parking Brake.............SET"},
-  {id:"21" ,value:"Exterior Lights...........................SET AS NEEDED"},
+  {id:"21" ,value:"Exterior Lights...............SET AS NEEDED"},
 
 
 
